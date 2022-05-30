3d5fb91868ebfd34b6987f0fec5493a956b18b09
chebok <vovakuzmin@gmail.com>
2022-05-30 13:59:00 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
-import { program } from "../node_modules/commander/esm.mjs";
+import { program } from 'commander';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 program.parse();
